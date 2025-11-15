7d9a0ead18f6cd2225927ab4e10eace976dc3fd9
royerrengifo01-png <royerrengifo01@gmail.com>
2025-11-14 23:15:04 -0500

    Agregada la de audio

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
     "http://localhost:5173",
     "https://podhub-frontend.onrender.com",
   ];
+
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
@@ -32,7 +33,10 @@ app.use((req, res, next) => {
   }
 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // 👇 ESTA LÍNEA ES LA CORRECTA
+  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
   if (req.method === "OPTIONS") return res.sendStatus(200);
